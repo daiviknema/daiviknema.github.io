7d85b8ed23a7520ee9a01b3442f5c0cb1a2c76da
daiviknema <daiviknema@gmail.com>
2020-09-23 23:52:44 +0530

    Set up Google Analytics - Analytics script is only visible in the html head for production build

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,9 +53,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-108266223-2`,
+        head: true,
+        anonymize: true,
+        respectDNT: true
       },
     },
+    // `gatsby-plugin-feed`,
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
